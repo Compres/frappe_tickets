4b726baf983a50626c3945a335d213692f4e5ddd
Dirk Chang <dirk@kooiot.com>
2017-03-06 16:10:16 +0800

    list issue

--- repair/repair/doctype/repair_issue/repair_issue.js
+++ repair/repair/doctype/repair_issue/repair_issue.js
@@ -2,6 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Repair Issue', {
+	setup: function(frm) {
+		frm.fields_dict["fixed_by"].get_query = function(){
+			return {
+				filters: {"ignore_user_type": 1}
+			};
+		};
+	},
 	refresh: function(frm) {
 
 	}
