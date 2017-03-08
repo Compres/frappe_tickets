1ee0401aa26a8a6d040b47295bed2ac90f1c361c
Dirk Chang <dirk@kooiot.com>
2017-03-08 17:45:24 +0800

    for indicator

--- repair/repair/doctype/repair_issue/repair_issue_list.js
+++ repair/repair/doctype/repair_issue/repair_issue_list.js
@@ -1,18 +1,8 @@
 frappe.listview_settings['Repair Issue'] = {
-	get_indicator: function(doc) {
-		if(doc.status == "New") {
-			return [__("New"), "red", "status,=,New"];
-		}
-		else if(doc.status == "Open") {
-			return [__("Open"), "orange", "status,=,Open"];
-		}
-		else if(doc.status == "Fixed") {
-			return [__("Fixed"), "green", "status,=,Fixed"];
-		}
-		else if(doc.status == "Closed") {
-			return [__("Closed"), "darkgrey", "status,=,Closed"];
-		}
-	},
+	/*get_indicator: function(doc) {
+		colour = {'New': 'red', 'Open': 'orange', 'Fixed': 'blue', 'Closed': 'darkgrey'};
+		return [__(doc.status), colour[doc.status], "status,=," + doc.status];
+	},*/
 	onload: function(me) {
 		frappe.route_options = {
 			"status": "Open"
