0f258da2f0c820f9dbc7e59653bdcebd8a52c1ee
Dirk Chang <dirk@kooiot.com>
2017-03-08 14:19:03 +0800

    list view of Repair Issue

--- repair/repair/doctype/repair_issue/repair_issue_list.js
+++ repair/repair/doctype/repair_issue/repair_issue_list.js
@@ -5,17 +5,7 @@ frappe.listview_settings['Repair Issue'] = {
 			"status": "Open"
 		};
 	},
-	hide_name_column: true,
 	refresh: function(me) {
-		// add assigned by me
-		me.page.add_sidebar_item(__("Assigned By Me"), function() {
-			var assign_filter = me.filter_list.get_filter("owner");
-			assign_filter && assign_filter.remove(true);
-
-			me.filter_list.add_filter(me.doctype, "assigned_by", '=', user);
-			me.run();
-		}, ".assigned-to-me");
-
 		// add created by me
 		me.page.add_sidebar_item(__("Created By Me"), function() {
 			var assign_filter = me.filter_list.get_filter("owner");
