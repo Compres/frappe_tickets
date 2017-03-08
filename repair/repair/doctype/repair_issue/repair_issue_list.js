75a1527756446260a9346fb96e5b504acaa5d7d8
Dirk Chang <dirk@kooiot.com>
2017-03-08 14:04:38 +0800

    list view of Repair Issue

--- repair/repair/doctype/repair_issue/repair_issue_list.js
+++ repair/repair/doctype/repair_issue/repair_issue_list.js
@@ -7,15 +7,6 @@ frappe.listview_settings['Repair Issue'] = {
 	},
 	hide_name_column: true,
 	refresh: function(me) {
-		// override assigned to me by owner
-		me.page.sidebar.find(".assigned-to-me a").off("click").on("click", function() {
-			var assign_filter = me.filter_list.get_filter("assigned_by");
-			assign_filter && assign_filter.remove(true);
-
-			me.filter_list.add_filter(me.doctype, "owner", '=', user);
-			me.run();
-		});
-
 		// add assigned by me
 		me.page.add_sidebar_item(__("Assigned By Me"), function() {
 			var assign_filter = me.filter_list.get_filter("owner");
@@ -24,6 +15,15 @@ frappe.listview_settings['Repair Issue'] = {
 			me.filter_list.add_filter(me.doctype, "assigned_by", '=', user);
 			me.run();
 		}, ".assigned-to-me");
+
+		// add created by me
+		me.page.add_sidebar_item(__("Created By Me"), function() {
+			var assign_filter = me.filter_list.get_filter("owner");
+			assign_filter && assign_filter.remove(true);
+
+			me.filter_list.add_filter(me.doctype, "owner", '=', user);
+			me.run();
+		}, ".created-to-me");
 	},
 	add_fields: ["issue_source_type", "issue_source"],
 }
