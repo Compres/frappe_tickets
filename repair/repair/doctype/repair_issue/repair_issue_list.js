ba89cdd992a859c0488fec017152ce9cc1911b56
Dirk Chang <dirk@kooiot.com>
2017-03-08 17:36:41 +0800

    show status indicator correctly

--- repair/repair/doctype/repair_issue/repair_issue_list.js
+++ repair/repair/doctype/repair_issue/repair_issue_list.js
@@ -1,4 +1,18 @@
 frappe.listview_settings['Repair Issue'] = {
+	get_indicator: function(doc) {
+		if(doc.status == "New") {
+			return [__("New"), "red", "status,=,New"];
+		}
+		else if(doc.status == "Open") {
+			return [__("Open"), "orange", "status,=,Open"];
+		}
+		else if(doc.status == "Fixed") {
+			return [__("Fixed"), "green", "status,=,Fixed"];
+		}
+		else if(doc.status == "Closed") {
+			return [__("Closed"), "darkgrey", "status,=,Closed"];
+		}
+	},
 	onload: function(me) {
 		frappe.route_options = {
 			"status": "Open"
