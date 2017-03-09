cca693171423b9136a83d6b1542b0ed69c07a160
Dirk Chang <dirk@kooiot.com>
2017-03-09 17:28:33 +0800

    make sure user has correct permission

--- repair/repair/doctype/repair_teamuser/repair_teamuser.py
+++ repair/repair/doctype/repair_teamuser/repair_teamuser.py
@@ -7,4 +7,12 @@ import frappe
 from frappe.model.document import Document
 
 class RepairTeamUser(Document):
-	pass
+	def validate(self):
+		if 'Repair Enterprise Admin' in frappe.get_roles(self.user):
+			frappe.throw(frappe._("User {0} is an Enterprise Admin").format(self.user))
+
+	def after_insert(self):
+		# Make sure it has Repair User permission
+		if 'Repair User' not in frappe.get_roles(self.user):
+			user = frappe.get_doc('User', self.user)
+			user.add_roles('Repair User')
