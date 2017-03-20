fd0c88091781cf52c24ce1fdb4fd1e8a937d3a1b
Dirk Chang <dirk@kooiot.com>
2017-03-20 13:04:59 +0800

    using select instead of link

--- repair/repair/doctype/repair_issue/repair_issue.js
+++ repair/repair/doctype/repair_issue/repair_issue.js
@@ -8,13 +8,13 @@ frappe.ui.form.on('Repair Issue', {
 				filters: {"ignore_user_type": 1}
 			};
 		};
-		frm.fields_dict["issue_source_type"].get_query = function(){
+		/* frm.fields_dict["issue_source_type"].get_query = function(){
 			return {
 				filters: {
 					"name": ["in","IOT Device Error,Repair Issue,IOT Device"]
 				}
 			}
-		};
+		}; */
 	},
 	refresh: function(frm) {
 
