bc9ade9b46143f6335e6bef16c79891178cb6cc0
Dirk Chang <dirk@kooiot.com>
2017-03-22 11:48:46 +0800

    ticket fix workflow

--- repair/repair/doctype/repair_ticket/repair_ticket.js
+++ repair/repair/doctype/repair_ticket/repair_ticket.js
@@ -5,7 +5,7 @@ frappe.ui.form.on('Repair Ticket', {
 	refresh: function(frm) {
 		if(frm.doc.docstatus == 1 && frm.doc.status=='New') {
 			frm.add_custom_button(__("Get It"), function() {
-				frm.events.ticket_get(frm);
+				frm.events.ticket_event(frm, "ticket_get");
 			});
 			frm.custom_buttons[__("Get It")].removeClass("btn-default");
 			frm.custom_buttons[__("Get It")].addClass("btn-primary");
@@ -24,7 +24,7 @@ frappe.ui.form.on('Repair Ticket', {
 			frm.custom_buttons[__("Create Report")].addClass("btn-primary");
 
 			frm.add_custom_button(__("Fixed"), function() {
-				frm.events.ticket_fixed(frm);
+				frm.events.ticket_event(frm, "ticket_fixed");
 			});
 			frm.custom_buttons[__("Fixed")].removeClass("btn-default");
 			frm.custom_buttons[__("Fixed")].addClass("btn-success");
@@ -32,13 +32,13 @@ frappe.ui.form.on('Repair Ticket', {
 		if(frm.doc.docstatus == 1 && frm.doc.status=='Fixed') {
 			if(has_common(roles, ["Administrator", "System Manager", "IOT Manager"]) && !frm.doc.__islocal) {
 				frm.add_custom_button(__("Close"), function() {
-					frm.events.ticket_close(frm);
+					frm.events.ticket_event(frm, "ticket_close");
 				});
 				frm.custom_buttons[__("Close")].removeClass("btn-default");
 				frm.custom_buttons[__("Close")].addClass("btn-success");
 
 				frm.add_custom_button(__("Reject"), function() {
-					frm.events.ticket_close(frm);
+					frm.events.ticket_event(frm, "ticket_reject");
 				});
 				frm.custom_buttons[__("Reject")].removeClass("btn-default");
 				frm.custom_buttons[__("Reject")].addClass("btn-warning");
@@ -46,47 +46,16 @@ frappe.ui.form.on('Repair Ticket', {
 		}
 	},
 
-	ticket_get: function(frm) {
+	ticket_event: function(frm, event) {
 		return frappe.call({
 			doc: frm.doc,
-			method: "ticket_get",
+			method: event,
 			freeze: true,
 			callback: function(r) {
-				if(!r.exc) frm.refresh_fields();
+				if(!r.exc)
+					frm.refresh_fields();
+					frm.refresh(frm);
 			}
 		})
 	},
-
-	ticket_fixed: function(frm) {
-		return frappe.call({
-			doc: frm.doc,
-			method: "ticket_fixed",
-			freeze: true,
-			callback: function(r) {
-				if(!r.exc) frm.refresh_fields();
-			}
-		})
-	},
-
-	ticket_close: function(frm) {
-		return frappe.call({
-			doc: frm.doc,
-			method: "ticket_close",
-			freeze: true,
-			callback: function(r) {
-				if(!r.exc) frm.refresh_fields();
-			}
-		})
-	},
-
-	ticket_reject: function(frm) {
-		return frappe.call({
-			doc: frm.doc,
-			method: "ticket_reject",
-			freeze: true,
-			callback: function(r) {
-				if(!r.exc) frm.refresh_fields();
-			}
-		})
-	}
 });
