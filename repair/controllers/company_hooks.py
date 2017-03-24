2632964359d38d06f59098c72462c55c87b987b7
Dirk Chang <dirk@kooiot.com>
2017-03-24 14:38:08 +0800

    add roles

--- repair/controllers/company_hooks.py
+++ repair/controllers/company_hooks.py
@@ -1,10 +1,11 @@
 from __future__ import unicode_literals
+import frappe
 
 
 def on_admin_insert(doc, method, user):
-	doc.add_roles('Repair Admin')
+	frappe.get_doc('User', user).add_roles('Repair Admin')
 
 
 def on_admin_remove(doc, method, user):
-	doc.remove_roles('Repair Admin')
+	frappe.get_doc('User', user).remove_roles('Repair Admin')
 
