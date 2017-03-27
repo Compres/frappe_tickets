14d55127f0f5323912adfe66c836e950be73168b
Dirk Chang <dirk@kooiot.com>
2017-03-27 15:04:01 +0800

    query team

--- tickets/tickets/doctype/tickets_site/tickets_site.js
+++ tickets/tickets/doctype/tickets_site/tickets_site.js
@@ -5,7 +5,7 @@ frappe.ui.form.on('Tickets Site', {
 	setup: function(frm) {
 		frm.fields_dict['team_assigned'].grid.get_field("team").get_query = function(){
 			return {
-				query:"tickets.tickets.doctype.tickets_site.tickets_site.team_query"
+				query:"tickets.tickets.doctype.tickets_site.tickets_site.query_team"
 			}
 		}
 	},
