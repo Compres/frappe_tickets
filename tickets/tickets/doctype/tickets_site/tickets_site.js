093677447615ae0152917327dfe1de517925ebcd
Dirk Chang <dirk@kooiot.com>
2017-03-27 14:27:07 +0800

    for query filter

--- tickets/tickets/doctype/tickets_site/tickets_site.js
+++ tickets/tickets/doctype/tickets_site/tickets_site.js
@@ -2,6 +2,13 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Tickets Site', {
+	setup: function(frm) {
+		frm.fields_dict['team_assigned'].grid.get_field("team").get_query = function(){
+			return {
+				filters: {"ignore_permissions": 1}
+			}
+		}
+	},
 	refresh: function(frm) {
 
 	}
